4b681daeef9414ac2e6e3ae7db5810f1eb33a1a2
Chizxsy <cfischer2247@gmail.com>
2025-09-22 14:14:23 -0600

    added server code from aesd

---
+++ client.c
@@ -0,0 +1,17 @@
+#include <syslog.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <sys/queue.h>
+#include <time.h>
+
