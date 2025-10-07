92a389cce97fa6c0175eb068216fa0ad70e2f40b
Chizxsy <cfischer2247@gmail.com>
2025-10-06 20:06:03 -0600

    fixed errors

--- Raid/raid5.c
+++ Raid/raid5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <syslog.h>
 
 #define buffer_size 1028
 
@@ -8,10 +9,10 @@ int store_image(char *input_file, size_t chunk_size, char *output_file){
     int chunk_num = 0;
 
     // open source image
-    FILE *fp_input = open(input_file, "rb");
+    FILE *fp_input = fopen(input_file, "rb");
     if (fp_input == NULL){
         syslog(LOG_ERR, "Failed to open input file");
-        return 1;
+        return -1;
     }
 
     // determine file size
@@ -27,33 +28,72 @@ int store_image(char *input_file, size_t chunk_size, char *output_file){
     char *buffer = malloc(chunk_size);
     if (buffer == NULL){
         syslog(LOG_INFO, "Failed to allocate memory");
-        fclose(fp);
+        fclose(fp_input);
     }
 
     // loop through all bytes and separate into chunks
-    while (file_size > 0){
+    long bytes_rem = file_size;
+    while (bytes_rem > 0){
+        // check number of remainging bytes
+        size_t num_bytes = (bytes_rem < (long)chunk_size) ? (size_t)bytes_rem : chunk_size;
+        // read remaining bytes
+        size_t bytes_rem = fread(buffer, 1, num_bytes, fp_input);
+
+        if (num_bytes != bytes_rem){
+            syslog(LOG_ERR, "Failed to read byte");
+            fclose(fp_input);
+            free(buffer);
+        }
+
         char output_filename[buffer_size];
         snprintf(output_filename, sizeof(output_filename), "chunk_%s.bin", output_file, chunk_num);
 
-        FILE *fp_output = open(output_file, "wb");
+        FILE *fp_output = fopen(output_file, "wb");
         if (fp_output == NULL){
             syslog(LOG_ERR, "Failed to open output file");
-            return 1;
+            free(buffer);
+            fclose(fp_input);
+            return -1;
         }
 
+        size_t bytes_written = fwrite(buffer, 1, bytes_rem, fp_output);
 
+        if (bytes_written != bytes_rem){
+            syslog(LOG_ERR, "Failed to write to output file");
+            free(buffer);
+            fclose(fp_output);
+            fclose(fp_input);
+            return -1;
+        }
 
-
+        fclose(fp_output);  
+        num_bytes -= bytes_rem;
         chunk_num++;
     }
 
     // clean up
-    syslog(LOG_INFO, "File splitting complete. Total chunks created: %d\n", chunk_number);
+    syslog(LOG_INFO, "File splitting complete. Total chunks created: %d\n", chunk_num);
     free(buffer);
-    fclose(f_in);
+    fclose(fp_input);
 }
 
 
-int main(int argc, char argv[]){
+int main(int argc, char *argv[]){
     openlog("Raid5", LOG_PID, LOG_USER);
+
+    if (argc != 4){
+        syslog(LOG_ERR, "Incorrect number of arguments");
+        return 1;
+    }
+
+    char *input_file = &argv[1];
+    size_t chunk_size = (size_t)argv[2];
+    char *output_dir = &argv[3];
+
+    int split_file = store_image(input_file, chunk_size, output_dir);
+    if (split_file > 0){
+        syslog(LOG_ERR, "Failed to store image");
+    }
+
+
 }
